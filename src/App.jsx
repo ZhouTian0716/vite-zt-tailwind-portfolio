16d974b6981ed08a1aa34c97e54a3e48eda6ea1c
ZhouTian0716 <egglord0716@gmail.com>
2022-12-05 00:17:28 +1100

    portfolio wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./scenes/Navbar";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isDesktop = useMediaQuery("(min-width: 1060px)");
 
-export default App
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/SocialMedialcons.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SocialMedialcons = () => {
+  return (
+    <div>SocialMedialcons</div>
+  )
+}
+
+export default SocialMedialcons
